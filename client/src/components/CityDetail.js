3c89c83b8cfde69609c3597a1f0adfbdecdfbe4b
VivianSolide <vivian.sarazin@gmail.com>
2018-07-29 13:57:03 +0200

    clipboard feature

--- client/src/components/CityDetail.js
+++ client/src/components/CityDetail.js
@@ -9,6 +9,7 @@ class CityDetail extends Component {
 			camera: null
 		};
 	}
+
 	componentDidMount() {
 		api.getCamera(this.props.match.params.cityId).then(res => {
 			console.log(res);
@@ -20,12 +21,16 @@ class CityDetail extends Component {
 	render() {
 		const camera = this.state.camera;
 		return (
-			this.state.camera && (
+			camera &&
+			camera.lastPhoto && (
 				<div
 					className="container-fluid"
 					id="cityDetail"
 					style={{
 						backgroundImage: `url(${camera.lastPhoto})`,
+						backgroundRepeat: "no-repeat",
+						backgroundPosition: "center",
+						backgroundSize: "cover",
 						height: "100vh"
 					}}
 				>
@@ -37,71 +42,205 @@ class CityDetail extends Component {
 
 					<div className="heading-cityDetail">
 						<h1>{camera.location.title}</h1>
-						<i>Colors given by RGB</i>
 					</div>
 
 					<div className="container colors">
 						<div className="row">
 							{camera.palette.Vibrant && (
-								<div className="col-lg-3 card">
+								<div className="col-lg-3 col-6 card">
+									<div
+										onClick={() => {
+											let text = `rgb(${camera.palette.Vibrant._rgb[0]},${
+												camera.palette.Vibrant._rgb[1]
+											},${camera.palette.Vibrant._rgb[2]})`;
+											const el = document.createElement("textarea");
+											el.value = text;
+											document.body.appendChild(el);
+											el.select();
+											document.execCommand("copy");
+											document.body.removeChild(el);
+											alert(text + " clipped in your clipboard");
+										}}
+										className="card-color"
+										style={{
+											backgroundColor: `rgb(${camera.palette.Vibrant._rgb[0]},${
+												camera.palette.Vibrant._rgb[1]
+											},${camera.palette.Vibrant._rgb[2]})`
+										}}
+									/>
+
 									<div className="card-body">
 										<h3 className="card-title">Vibrant</h3>
 										{camera.palette.Vibrant._rgb.map((e, i) => {
-											return <p className="card-text">{e} </p>;
+											return <p className="card-text">{Math.floor(e)} </p>;
 										})}
 									</div>
 								</div>
 							)}
 
 							{camera.palette.LightVibrant && (
-								<div className="col-lg-3 card">
+								<div className="col-lg-3 col-6 card">
+									<div
+										onClick={() => {
+											let text = `rgb(${camera.palette.LightVibrant._rgb[0]},${
+												camera.palette.LightVibrant._rgb[1]
+											},${camera.palette.LightVibrant._rgb[2]})`;
+											const el = document.createElement("textarea");
+											el.value = text;
+											document.body.appendChild(el);
+											el.select();
+											document.execCommand("copy");
+											document.body.removeChild(el);
+											alert(text + " clipped in your clipboard");
+										}}
+										className="card-color"
+										style={{
+											backgroundColor: `rgb(${
+												camera.palette.LightVibrant._rgb[0]
+											},${camera.palette.LightVibrant._rgb[1]},${
+												camera.palette.LightVibrant._rgb[2]
+											})`
+										}}
+									/>
+
 									<div className="card-body">
 										<h3 className="card-title">LightVibrant</h3>
 										{camera.palette.LightVibrant._rgb.map((e, i) => {
-											return <p className="card-text">{e} </p>;
+											return <p className="card-text">{Math.floor(e)} </p>;
 										})}
 									</div>
 								</div>
 							)}
 
 							{camera.palette.DarkVibrant && (
-								<div className="col-lg-3 card">
+								<div className="col-lg-3 col-6 card">
+									<div
+										onClick={() => {
+											let text = `rgb(${camera.palette.DarkVibrant._rgb[0]},${
+												camera.palette.DarkVibrant._rgb[1]
+											},${camera.palette.DarkVibrant._rgb[2]})`;
+											const el = document.createElement("textarea");
+											el.value = text;
+											document.body.appendChild(el);
+											el.select();
+											document.execCommand("copy");
+											document.body.removeChild(el);
+											alert(text + " clipped in your clipboard");
+										}}
+										className="card-color"
+										style={{
+											backgroundColor: `rgb(${
+												camera.palette.DarkVibrant._rgb[0]
+											},${camera.palette.DarkVibrant._rgb[1]},${
+												camera.palette.DarkVibrant._rgb[2]
+											})`
+										}}
+									/>
+
 									<div className="card-body">
 										<h3 className="card-title">DarkVibrant</h3>
 										{camera.palette.DarkVibrant._rgb.map((e, i) => {
-											return <p className="card-text">{e} </p>;
+											return <p className="card-text">{Math.floor(e)} </p>;
 										})}
 									</div>
 								</div>
 							)}
+
 							{camera.palette.Muted && (
-								<div className="col-lg-3 card">
+								<div className="col-lg-3 col-6 card">
+									<div
+										onClick={() => {
+											let text = `rgb(${camera.palette.Muted._rgb[0]},${
+												camera.palette.Muted._rgb[1]
+											},${camera.palette.Muted._rgb[2]})`;
+											const el = document.createElement("textarea");
+											el.value = text;
+											document.body.appendChild(el);
+											el.select();
+											document.execCommand("copy");
+											document.body.removeChild(el);
+											alert(text + " clipped in your clipboard");
+										}}
+										className="card-color"
+										style={{
+											backgroundColor: `rgb(${camera.palette.Muted._rgb[0]},${
+												camera.palette.Muted._rgb[1]
+											},${camera.palette.Muted._rgb[2]})`
+										}}
+									/>
+
 									<div className="card-body">
 										<h3 className="card-title">Muted</h3>
 										{camera.palette.Muted._rgb.map((e, i) => {
-											return <p className="card-text">{e} </p>;
+											return <p className="card-text">{Math.floor(e)} </p>;
 										})}
 									</div>
 								</div>
 							)}
 
 							{camera.palette.LightMuted && (
-								<div className="col-lg-3 card">
+								<div className="col-lg-3 col-6 card">
+									<div
+										onClick={() => {
+											let text = `rgb(${camera.palette.LightMuted._rgb[0]},${
+												camera.palette.LightMuted._rgb[1]
+											},${camera.palette.LightMuted._rgb[2]})`;
+											const el = document.createElement("textarea");
+											el.value = text;
+											document.body.appendChild(el);
+											el.select();
+											document.execCommand("copy");
+											document.body.removeChild(el);
+											alert(text + " clipped in your clipboard");
+										}}
+										className="card-color"
+										style={{
+											backgroundColor: `rgb(${
+												camera.palette.LightMuted._rgb[0]
+											},${camera.palette.LightMuted._rgb[1]},${
+												camera.palette.LightMuted._rgb[2]
+											})`
+										}}
+									/>
+
 									<div className="card-body">
 										<h3 className="card-title">LightMuted</h3>
 										{camera.palette.LightMuted._rgb.map((e, i) => {
-											return <p className="card-text">{e} </p>;
+											return <p className="card-text">{Math.floor(e)} </p>;
 										})}
 									</div>
 								</div>
 							)}
 
 							{camera.palette.DarkMuted && (
-								<div className="col-lg-3 card">
+								<div className="col-lg-3 col-6 card">
+									<div
+										onClick={() => {
+											let text = `rgb(${camera.palette.DarkMuted._rgb[0]},${
+												camera.palette.DarkMuted._rgb[1]
+											},${camera.palette.DarkMuted._rgb[2]})`;
+											const el = document.createElement("textarea");
+											el.value = text;
+											document.body.appendChild(el);
+											el.select();
+											document.execCommand("copy");
+											document.body.removeChild(el);
+											alert(text + " clipped in your clipboard");
+										}}
+										className="card-color"
+										style={{
+											backgroundColor: `rgb(${
+												camera.palette.DarkMuted._rgb[0]
+											},${camera.palette.DarkMuted._rgb[1]},${
+												camera.palette.DarkMuted._rgb[2]
+											})`
+										}}
+									/>
+
 									<div className="card-body">
 										<h3 className="card-title">DarkMuted</h3>
 										{camera.palette.DarkMuted._rgb.map((e, i) => {
-											return <p className="card-text">{e} </p>;
+											return <p className="card-text">{Math.floor(e)} </p>;
 										})}
 									</div>
 								</div>
